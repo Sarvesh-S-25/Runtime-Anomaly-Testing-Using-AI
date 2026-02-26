7914c32896c92dcdc2b2c5767a4584f0e3b4ce9a
Sarvesh-S-25 <sarveshs2025@gmail.com>
2026-02-26 22:25:21 +0530

    Add files via upload

---
+++ automated_running/automate_exp2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+EXP=exp2
+RUNS=500
+
+for CPU in 1 2 4; do
+  mkdir -p logs/$EXP/cpu$CPU
+
+  for i in $(seq -w 1 $RUNS); do
+    echo "EXP2 | CPU=$CPU | RUN=$i"
+    ./run-qemu.sh $CPU | tee logs/$EXP/cpu$CPU/run_${i}.log
+  done
+done
+
